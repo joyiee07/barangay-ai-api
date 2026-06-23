084842185ab79b3fb1f34cf4ddd73125d280f9e3
joyiee07 <maestbulao07@gmail.com>
2026-06-23 08:24:27 +0800

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -6,7 +6,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import LinearSVC
 
 # Load dataset
-data = pd.read_csv("dataset_1000.csv")
+data = pd.read_csv("dataset.csv")
 
 X = data["description"]
 y = data["label"]
